f458578039e7fd96561b92669a29323ca6add6c2
Gia Huy Tran <tonytran0314@gmail.com>
2024-04-09 02:09:01 -0400

    add icons

--- src/components/card/column/DaysForecast.vue
+++ src/components/card/column/DaysForecast.vue
@@ -5,14 +5,19 @@
 <template>
     <div class="detail_item" id="days">
         <div class="title_container">
-            <div class="title_icon">icon</div>
-            <div class="title">10-DAY FORECAST</div>
+            <div class="title_icon">
+                <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3 9H21M7 3V5M17 3V5M6 12H8M11 12H13M16 12H18M6 15H8M11 15H13M16 15H18M6 18H8M11 18H13M16 18H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke="rgb(98, 116, 139)" stroke-width="2" stroke-linecap="round"/>
+                </svg>
+            </div>
+            <div class="title">DAILY FORECAST</div>
         </div>
         <div id="days_forecast">
             <!-- Start loop -->
             <div class="days_forecast_item">
                 <div class="date">Today</div>
-                <div class="day_weather_status_icon">icon</div>
+                <div class="day_weather_status_icon">
+                        <img src="/src/assets/images/clear.png" /></div>
                 <div class="tempurature_range">
                     <div class="day_highest_tempurature">38°</div>
                     <div class="tempurature_bar">bar</div>
@@ -23,7 +28,8 @@
             
             <div class="days_forecast_item">
                 <div class="date">Today</div>
-                <div class="day_weather_status_icon">icon</div>
+                <div class="day_weather_status_icon">
+                        <img src="/src/assets/images/clear.png" /></div>
                 <div class="tempurature_range">
                     <div class="day_highest_tempurature">38°</div>
                     <div class="tempurature_bar">bar</div>
@@ -33,7 +39,8 @@
             
             <div class="days_forecast_item">
                 <div class="date">Today</div>
-                <div class="day_weather_status_icon">icon</div>
+                <div class="day_weather_status_icon">
+                        <img src="/src/assets/images/clear.png" /></div>
                 <div class="tempurature_range">
                     <div class="day_highest_tempurature">38°</div>
                     <div class="tempurature_bar">bar</div>
@@ -43,7 +50,8 @@
             
             <div class="days_forecast_item">
                 <div class="date">Today</div>
-                <div class="day_weather_status_icon">icon</div>
+                <div class="day_weather_status_icon">
+                        <img src="/src/assets/images/clear.png" /></div>
                 <div class="tempurature_range">
                     <div class="day_highest_tempurature">38°</div>
                     <div class="tempurature_bar">bar</div>
@@ -71,8 +79,15 @@
             .days_forecast_item {
                 display: flex;
                 justify-content: space-between;
+                align-items: center;
                 border-top: 1px solid $labelColor;
-                padding: $baseDistance*1.5 0 $baseDistance 0;
+                padding-top: $baseDistance;
+
+                .day_weather_status_icon {
+                    img {
+                        width: $baseDistance * 4;
+                    }
+                }
 
                 .tempurature_range {
                     display: flex;
--- src/components/card/row/Precipitation.vue
+++ src/components/card/row/Precipitation.vue
@@ -5,7 +5,11 @@
 <template>
     <div class="detail_item row_item" id="precipitation">
         <div class="title_container">
-            <div class="title_icon">icon</div>
+            <div class="title_icon">
+                <svg fill="rgb(98, 116, 139)" width="16px" height="16px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M25.378 19.75c1.507 6.027-3.162 11.25-9.375 11.25s-10.9-5.149-9.375-11.25c0.937-3.75 5.625-9.375 9.375-18.75 3.75 9.374 8.438 15 9.375 18.75z"></path>
+                </svg>
+            </div>
             <div class="title">PRECIPITATION</div>
         </div>
         <div class="row_item_body">

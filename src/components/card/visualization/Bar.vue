81c862e58dba3c81445af9a0993d53d61dadbfe3
Gia Huy Tran <tonytran0314@gmail.com>
2024-04-10 02:10:34 -0400

    build temperature bar

--- src/components/card/visualization/Bar.vue
+++ src/components/card/visualization/Bar.vue
@@ -3,9 +3,12 @@
 </script>
 
 <template>
+
     <div class="bar_container">
         <div class="temp_range">
-            <div class="current_temp"></div>
+            <div class="gradient">
+                <div class="current_temp"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -16,25 +19,36 @@
 
     .bar_container {
         width: 100px;
-        height: 6px;
+        height: 8px;
         background-color: $labelColor;
         border-radius: $baseDistance;
+        position: relative;
 
         .temp_range {
             width: 56%;
             height: 100%;
-            // background-image: linear-gradient(to right, #AEC6CF, #AEC6CF, #77DD77, #77DD77, yellow, yellow, orange, orange, red, red);
-            background-image: linear-gradient(to right, green, yellow);
             border-radius: $baseDistance;
+            overflow: hidden;
             margin-left: 32%;
+            position: absolute;
 
-            .current_temp {
-                width: 6px;
+
+            .gradient {
+                width: 100px;
                 height: 100%;
-                background-color: white;
-                border-radius: 50%;
-                border: 1px solid black;
-                margin-left: 28%;
+                background-image: linear-gradient(to right, cyan, cyan, orange);
+                border-radius: $baseDistance;
+                position: absolute;
+                left: -56%;
+
+                .current_temp {
+                    width: 8px;
+                    height: 100%;
+                    background-color: white;
+                    border-radius: 50%;
+                    border: 1px solid black;
+                    margin-left: 55%;
+                }
             }
         }
     }
